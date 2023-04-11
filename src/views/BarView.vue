5ad59cd4c4e77884a31e896775f65da348c7dbfc
Mikhail Vasilev <mgp-nsk@yandex.ru>
2023-04-11 16:55:46 +0700

    barChart properties at finish line

--- src/views/BarView.vue
+++ src/views/BarView.vue
@@ -6,7 +6,7 @@
       <div>
         <pre>
 <code>chart.config: {
-  type: "doughnut",
+  type: "bar",
   data: {
     /* Базовый массив ярыков */
     <mark>labels: [ "Chrome", "Safari", "Samsung Internet", "Opera", "UC Browser", "Android", ]</mark>,
@@ -119,19 +119,69 @@
       </div>
     </div>
     <div class="display-flex flex-direction-column align-items-start">
-      <h4>Без параметров</h4>
+      <h4>Относительное положение</h4>
       <div class="chart-container"><canvas id="barChart6"></canvas></div>
       <div>
         <pre>
 <code>chart.config: {
-  /*  */
-  <mark></mark>
+  data: {
+    datasets: [
+      {
+        /* Ширина столбца в пикселях */
+        <mark>barThickness: 45,</mark>
+        /* Расположить столбцы рядом (true - по умолчанию) или один поверх другого, соосно (false) */
+        <mark>grouped: false,</mark>
+        /* Искуственное наращивание высоты столбца до минимального значения (в пикселях) */
+        <mark>minBarLength: 10,</mark>
+      },
+    ]
+  }
+},</code></pre>
+      </div>
+    </div>
+    <div class="display-flex flex-direction-column align-items-start">
+      <h4>Сгруппированные столбцы</h4>
+      <div class="chart-container"><canvas id="barChart7"></canvas></div>
+      <div>
+        <pre>
+<code>chart.config: {
+  data: {
+    datasets: [
+      {
+        /* Порядок расположения столбцов. Чем меньше число, тем ближе столбец к началу координат (по умолчанию - 0) */
+        <mark>order: 3,</mark>
+        /* Увеличение ширины столбцов с возможным наложением. */
+        /* Размер в пикселях. Значение по умолчанию - "auto" */
+        <mark>inflateAmount: 5,</mark>
+      },
+    ]
+  }
+},</code></pre>
+      </div>
+    </div>
+    <div class="display-flex flex-direction-column align-items-start">
+      <h4>Колоды (стек)</h4>
+      <div class="chart-container"><canvas id="barChart8"></canvas></div>
+      <div>
+        <pre>
+<code>chart.config: {
+  data: {
+    datasets: [
+      {
+        /* Идентификатор колоды. Если у двух и более массивов данных указан одинаковый идентификатор, то столбцы будут составлять колоду */
+        <mark>stack: "mobileAndDesktop",</mark>
+        /* Идентификатор осей (в одном чарте совмещаются две или более системы координат с разными идентификаторами, добавляются дополнительные шкалы) */
+        // <mark>xAxisID: "Rating",</mark>
+        <mark>yAxisID: "Brand",</mark>
+      },
+    ]
+  }
 },</code></pre>
       </div>
     </div>
     <div class="display-flex flex-direction-column align-items-start">
       <h4>Без параметров</h4>
-      <div class="chart-container"><canvas id="barChart7"></canvas></div>
+      <div class="chart-container"><canvas id="barChart9"></canvas></div>
       <div>
         <pre>
 <code>chart.config: {
@@ -142,7 +192,7 @@
     </div>
     <div class="display-flex flex-direction-column align-items-start">
       <h4>Без параметров</h4>
-      <div class="chart-container"><canvas id="barChart8"></canvas></div>
+      <div class="chart-container"><canvas id="barChart10"></canvas></div>
       <div>
         <pre>
 <code>chart.config: {
@@ -277,6 +327,8 @@ export default {
                 clip: -20,
                 indexAxis: "y",
                 label: "Mobile Browser Rating",
+                xAxisID: "Rating",
+                yAxisID: "Brand",
               },
             ],
           },
@@ -302,6 +354,10 @@ export default {
               {
                 label: "Mobile",
                 data: [64.5, 25, 0, 0, 4.5, 2, 1.5, 0.5, 0, 2],
+                barThickness: 15,
+                // grouped: false,
+                minBarLength: 10,
+                order: 2,
                 // barPercentage: 1,
                 // inflateAmount: 5,
                 // categoryPercentage: 0.5,
@@ -310,6 +366,10 @@ export default {
               {
                 label: "Desktop",
                 data: [66, 11, 11, 6.5, 0, 3, 0, 0, 0.5, 2],
+                barThickness: 45,
+                // grouped: false,
+                minBarLength: 10,
+                order: 3,
                 // barPercentage: 1,
                 // inflateAmount: 5,
                 // inflateAmount: 1,
@@ -318,12 +378,84 @@ export default {
               },
             ],
           },
-          options: {
-            skipNull: true,
-          },
         },
       },
       barChart7: {
+        config: {
+          type: "bar",
+          data: {
+            labels: [
+              "Chrome",
+              "Safari",
+              "Edge",
+              "Firefox",
+              "Samsung Internet",
+              "Opera",
+              "UC Browser",
+              "Android",
+              "Internet Explorer",
+              "Others",
+            ],
+            datasets: [
+              {
+                label: "Mobile",
+                data: [64.5, 25, 0, 0, 4.5, 2, 1.5, 0.5, 0, 2],
+                order: 2,
+                pointStyle: "circle",
+                // barPercentage: 1,
+                inflateAmount: 5,
+                // categoryPercentage: 0.5,
+                // skipNull: false,
+              },
+              {
+                label: "Desktop",
+                data: [66, 11, 11, 6.5, 0, 3, 0, 0, 0.5, 2],
+                order: 1,
+                pointStyle: "circle",
+                // barPercentage: 1,
+                inflateAmount: 5,
+                // inflateAmount: 1,
+                // categoryPercentage: 0.5,
+                // skipNull: false,
+              },
+            ],
+          },
+        },
+      },
+      barChart8: {
+        config: {
+          type: "bar",
+          data: {
+            labels: [
+              "Chrome",
+              "Safari",
+              "Edge",
+              "Firefox",
+              "Samsung Internet",
+              "Opera",
+              "UC Browser",
+              "Android",
+              "Internet Explorer",
+              "Others",
+            ],
+            datasets: [
+              {
+                label: "Mobile",
+                data: [64.5, 25, 0, 0, 4.5, 2, 1.5, 0.5, 0, 2],
+                // xAxisID: "Rating",
+                yAxisID: "mobileBrand",
+              },
+              {
+                label: "Desktop",
+                data: [20, 20, 15, 15, 0, 15, 0, 0, 5, 10],
+                // xAxisID: "Rating",
+                yAxisID: "desktopBrand",
+              },
+            ],
+          },
+        },
+      },
+      barChart9: {
         config: {
           type: "bar",
           data: {
@@ -343,7 +475,7 @@ export default {
           },
         },
       },
-      barChart8: {
+      barChart10: {
         config: {
           type: "bar",
           data: {
@@ -374,6 +506,13 @@ export default {
     new Chart(document.getElementById("barChart6"), this.barChart6.config);
     new Chart(document.getElementById("barChart7"), this.barChart7.config);
     new Chart(document.getElementById("barChart8"), this.barChart8.config);
+    new Chart(document.getElementById("barChart9"), this.barChart8.config);
+    new Chart(document.getElementById("barChart10"), this.barChart8.config);
+    // Осталось
+    //   categoryPercentage
+    //   maxBarThickness
+    //   pointStyle
+    //   skipNull
   },
 };
 </script>
